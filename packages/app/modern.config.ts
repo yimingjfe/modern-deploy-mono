2068dbd655cac521fdbec363192758e7a87c9d49
wangyiming <wangyiming.fe@bytedance.com>
2024-05-16 15:42:33 +0800

    feat: change to custom server and add netlify.toml

--- packages/app/modern.config.ts
+++ packages/app/modern.config.ts
@@ -1,6 +1,5 @@
 import { appTools, defineConfig } from '@modern-js/app-tools';
-import { bffPlugin } from '@modern-js/plugin-bff';
-import { koaPlugin } from '@modern-js/plugin-koa';
+import { serverPlugin } from '@modern-js/plugin-server';
 
 // https://modernjs.dev/en/configure/app/usage
 export default defineConfig({
@@ -17,7 +16,6 @@ export default defineConfig({
     appTools({
       bundler: 'experimental-rspack',
     }),
-    bffPlugin(),
-    koaPlugin(),
+    serverPlugin(),
   ],
 });

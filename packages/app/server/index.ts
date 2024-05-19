05f96f14f2b6a9d3f90a3e3572c01673352f93af
wangyiming <wangyiming.fe@bytedance.com>
2024-05-19 22:21:17 +0800

    feat: custom server and netlify

--- packages/app/server/index.ts
+++ packages/app/server/index.ts
@@ -2,9 +2,9 @@ import { Middleware } from '@modern-js/runtime/server';
 import { add } from 'utils'
 
 export const middleware: Middleware = (context, next) => {
-  const {
-    source: { req, res },
-  } = context;
+  // const {
+  //   source: { req, res },
+  // } = context;
   console.log('custom middleware', add(8, 9));
   next();
 };
